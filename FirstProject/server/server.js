040b94c653cafeb9d795a8f4a78396fed559e145
Barros7 <josebarros412@gmail.com>
2022-05-28 04:01:19 +0100

    form validation

--- FirstProject/server/server.js
+++ FirstProject/server/server.js
@@ -1,43 +1,48 @@
 const express = require('express');
 const path = require('path');
 const myDatabase = require('./config/database');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 3006;
 
 const app = express();
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+let user = [
+    porto = {
+        estadio: 'Dragao',
+        quantidade: 300000
+    },
+    benfica = {
+        estadio: 'Lúz',
+        quantidade: 300000
+    }
+];
+
 app.get('/', (request, response) => {
-    response.json(
-        [
-            aluno1 = {
-                "nome": "Madeira",
-                "email": "Madeira",
-                "senha": "Madeira",
-            },
-            aluno2 = {
-                "nome": "Madeira",
-                "email": "Madeira",
-                "senha": "Madeira",
-            }
-        ]
+    response.send(
+       user
     );
 
 });
 
 app.post('/registo', (request, response) => {
 
-    //alert('Registo')
     let username = request.body.username;
     let email = request.body.email;
     let senha = request.body.senha;
+
+    console.log(username, "\n", email, "\n", senha);
+    //user.push('username', 'email', 'senha');
 
     response.json({
             nome: username,
             email: email,
             senha: senha,
-        })
-        /*
+        })/*
         myDatabase.query('INSERT INTO SET ? ', {
             nome: username,
             email: email,
