176ee30eccfcef4ee14998c13aa30e49d8054547
Suraia2001 <Indiramadeira17@gmail.com>
2022-05-29 20:23:47 +0100

    validação do login e design da página de questões

--- FirstProject/server/server.js
+++ FirstProject/server/server.js
@@ -3,7 +3,10 @@ const path = require('path');
 const myDatabase = require('./config/database');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const sha = require('sha.js');
+const salt = 12;
 const port = 3006;
+
 
 const app = express();
 app.use(cors());
@@ -11,44 +14,44 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-let user = [
-    porto = {
-        estadio: 'Dragao',
-        quantidade: 300000
-    },
-    benfica = {
-        estadio: 'Lúz',
-        quantidade: 300000
-    }
-];
-
 app.get('/', (request, response) => {
-    response.send(
-       user
-    );
+    response.send();
 
 });
+app.post('/login', async(request, response) => {
+    let email = request.body.email;
+    let senha = request.body.senha;
 
-app.post('/registo', (request, response) => {
+    if (email && senha) {
+        myDatabase.query('SELECT * FROM utilizador WHERE email = ? ', [email], async(error, results, fields) => {
+            if (results.length > 0) {
+                let sha256 = sha('sha256');
+                let passwordHashed = sha256.update(senha + salt).digest("hex");
+                if (results.length == 0 || !(passwordHashed == results[0].senha)) {
+                    console.log('O nome do utilador ou a password está errada')
+                } else {
+                    console.log('login feito com sucesso')
+                }
+            }
+        });
+    }
+});
+
+app.post('/registo', async(request, response) => {
 
     let username = request.body.username;
     let email = request.body.email;
     let senha = request.body.senha;
 
-    console.log(username, "\n", email, "\n", senha);
-    //user.push('username', 'email', 'senha');
+    let sha256 = sha("sha256");
+    let passwordHashed = sha256.update(senha + salt).digest("hex");
 
-    response.json({
-            nome: username,
-            email: email,
-            senha: senha,
-        })/*
-        myDatabase.query('INSERT INTO SET ? ', {
-            nome: username,
-            email: email,
-            senha: senha,
+    myDatabase.query('INSERT INTO utilizador SET ? ', {
+        nome: username,
+        email: email,
+        senha: passwordHashed,
 
-        });*/
+    });
 });
 
 app.listen(port, () => {
